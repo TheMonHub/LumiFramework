2ff7f3adbadaa6654460ef479520c1c19aedd6c9
TheMonHub <mono15.parnthakote@gmail.com>
2025-05-23 20:50:16 +0700

    Initial #2

---
+++ src/test.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Mono on 23/5/2025.
+//
+
+#include "test.h"
+
+#include <iostream>
+
+void test::testing() {
+	std::cout << "test" << std::endl;
+}
+
+int main() {
+	test::testing();
+	return 0;
+}
