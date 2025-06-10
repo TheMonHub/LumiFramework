aa4ed14b7782d92b02deff862de64b371caac3ec
TheMonHub <80322542+TheMonHub@users.noreply.github.com>
2025-06-10 07:14:42 +0000

    Automated: Run clang-format script

--- examples/main.cpp
+++ examples/main.cpp
@@ -16,6 +16,8 @@ int main() {
 	Lumi::ErrorHandler::LUMI_SET_FUNNY_ERROR_BOOL(true);
 	Lumi::ErrorHandler::LUMI_SET_FATAL_SEVERITY(Lumi::ErrorHandler::LogSeverity::Warning);
 	Lumi::ErrorHandler::LUMI_REGISTER_DEFAULT_FUNNY_MESSAGES();
-	std::cout << "\n" << "Lumi Version: " << Lumi::Info::Version::GetVersionString(true) << "\n" << std::endl;
+	std::cout << "\n"
+			  << "Lumi Version: " << Lumi::Info::Version::GetVersionString(true) << "\n"
+			  << std::endl;
 	std::cout << Lumi::Info::License::GetLicenseString() << "\n" << std::endl;
 }
