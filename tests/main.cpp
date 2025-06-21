2723199e95317d29b33df151effcde33a5714912
TheMonHub <mono15.parnthakote@gmail.com>
2025-06-21 14:29:00 +0700

    work?it should work

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2025.
 // Distributed under the Boost Software License, Version 1.0
 
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
 
 TEST(PlaceholderTest, MinimalPass) {
     ASSERT_TRUE(true);
