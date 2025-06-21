76b00a4c4e8fb09b9452e424715d6cf34c939ef0
TheMonHub <mono15.parnthakote@gmail.com>
2025-06-21 21:23:11 +0700

    nope try again

--- clang-format.sh
+++ clang-format.sh
@@ -3,4 +3,4 @@
 
 # shellcheck disable=SC2035
 shopt -s globstar
-find . -type f \( -name "*.cpp" -o -name "*.h" \) -not -path "*/_deps/*" -not -path "*/CMakeFiles/*" -exec clang-format -i -style=file --dry-run --Werror --verbose {} +
+find . -type f \( -name "*.cpp" -o -name "*.h" \) -not -path "*/_deps/*" -not -path "*/llvm/*" -exec clang-format -i -style=file --dry-run --Werror --verbose {} +
