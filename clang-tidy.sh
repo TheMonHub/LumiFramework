717f8c3b492ce1c423be27fde98403cc3877b4ae
TheMonHub <mono15.parnthakote@gmail.com>
2025-06-29 11:26:04 +0700

    fix: window ci lint fail

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -2,7 +2,7 @@
 # Licensed under zlib License
 
 # shellcheck disable=SC2046
-cmake -S . -B build -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DLUMI_BUILD_TESTS=ON -DLUMI_BUILD_EXAMPLES=ON -DLUMI_BUILD_ALL=ON -DLUMI_BUILD_DOCS=OFF -D CMAKE_C_COMPILER=$(which clang) -D CMAKE_CXX_COMPILER=$(which clang++) -G Ninja
+cmake -S . -B build -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DLUMI_BUILD_TESTS=ON -DLUMI_BUILD_EXAMPLES=ON -DLUMI_BUILD_ALL=ON -DLUMI_BUILD_DOCS=OFF -D CMAKE_C_COMPILER="$(which clang)" -D CMAKE_CXX_COMPILER="$(which clang++)" -G Ninja
 cmake --build build
 # shellcheck disable=SC2046
 run-clang-tidy '^(?!.*[\\/]_deps[\\/])' -p build -quiet
